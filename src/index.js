530dda32768a0b70493e720b3ffb454bce47a593
Shirsha Sengupta <shirsha.21ug2036@iiitranchi.ac.in>
2024-03-12 23:46:56 +0530

    Addition

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const createAndSetupServer = async()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-    app.use(cors({origin:"http://localhost:3000", credentials:true}));
+    app.use(cors({origin:"http://localhost:5173", credentials:true}));
     app.use(cookieParser());
     app.use("/api", apiRoomRoutes);
     app.use("/api", apiBookingRoutes);
